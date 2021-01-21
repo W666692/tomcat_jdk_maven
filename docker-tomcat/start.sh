a0d39859094f2f156ed7c95ebc3069e2f07b8621
dangqili <lyz880104@live.cn>
2021-01-21 10:25:57 +0800

    docker-tomcat

---
+++ docker-tomcat/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# start 1
+nginx -g "daemon off;"  > /var/log/start1.log 2>&1 &
+# start 2
+/usr/local/tomcat8/bin/catalina.sh run  > /var/log/start2.log 2>&1 &
+# start 3
+/usr/sbin/init  > /var/log/start3.log 2>&1 &
+# just keep this script running
+while [[ true ]]; do
+    sleep 1
+done
